7f9dc3129d94248690f360440354b9b68d8ce52c
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2021-07-08 15:54:43 -0300

    Update add-share-file-volume.sh

--- moodle-aws/scripts/add-share-file-volume.sh
+++ moodle-aws/scripts/add-share-file-volume.sh
@@ -5,5 +5,4 @@ sudo yum install amazon-efs-utils -y
 
 sudo mkdir /moodlesite
 sudo mount -t efs -o tls,accesspoint=fsap-0dad50e0fc42eedef fs-b83bd2a0:/ /moodlesite
-
 sudo bash -c 'echo "fs-b83bd2a0:/ /moodlesite efs defaults,_netdev 0 0" >> /etc/fstab'
