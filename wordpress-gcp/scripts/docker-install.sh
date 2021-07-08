0a1f0df73f33a164e9738efd611a39023e0d4418
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2021-07-08 15:09:41 -0300

    Update docker-install.sh

--- wordpress-gcp/scripts/docker-install.sh
+++ wordpress-gcp/scripts/docker-install.sh
@@ -6,7 +6,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io -y
-sudo adduser suporte docker
 
 # Instala o Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/sbin/docker-compose
