9e3f970d5338f2fa1d234cf20d03f31257093ed5
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2021-07-12 18:03:43 -0300

    Add files via upload

---
+++ moodle-aws/terraform/instance.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "webserver-moodle-terraform" {
+  ami 		= var.ami
+  instance_type = var.instance_type
+  subnet_id 	= var.subnet_id
+  vpc_security_group_ids = [ var.vpc_security_group_ids ]
+  key_name	= var.key_name
+  user_data	= file("files/webserver-moodle-install.sh")
+  tags = {
+    Name        = var.name
+  }
+}
