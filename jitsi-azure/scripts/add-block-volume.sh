f0f5eb787dc352e254813094b43a0d290e5c505f
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2021-09-11 15:11:30 -0300

    Update add-block-volume.sh

--- jitsi-azure/scripts/add-block-volume.sh
+++ jitsi-azure/scripts/add-block-volume.sh
@@ -4,6 +4,6 @@
 sudo mkdir /jitsi-meet-confs
 
 # Monta disco para gravar dados das confs
-sudo mkfs.xfs /dev/sdc 
-sudo mount -t xfs /dev/sdc /jitsi-meet-confs
-sudo bash -c 'echo "/dev/sdc /jitsi-meet-confs xfs defaults 0 0" >> /etc/fstab'
+sudo mkfs.xfs /dev/sda 
+sudo mount -t xfs /dev/sda /jitsi-meet-confs
+sudo bash -c 'echo "/dev/sda /jitsi-meet-confs xfs defaults 0 0" >> /etc/fstab'
