4f471ee432715b13d06913ecf969b7c277c0c75f
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2021-09-12 11:54:23 -0300

    Update dbserver-webserver-install.sh

--- wordpress-gcp/userdata/dbserver-webserver-install.sh
+++ wordpress-gcp/userdata/dbserver-webserver-install.sh
@@ -6,7 +6,7 @@ apt install apt-transport-https ca-certificates curl gnupg-agent software-proper
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
-apt install docker-ce docker-ce-cli containerd.io git -y
+apt install docker-ce docker-ce-cli containerd.io git vim -y
 
 # Instala o Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/sbin/docker-compose
