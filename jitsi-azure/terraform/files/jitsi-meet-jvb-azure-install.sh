9b192dd11c7e320d7a349147f95d0201a3078140
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2021-09-27 10:57:36 -0300

    Update jitsi-meet-jvb-azure-install.sh

--- jitsi-azure/terraform/files/jitsi-meet-jvb-azure-install.sh
+++ jitsi-azure/terraform/files/jitsi-meet-jvb-azure-install.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 
-# Cria diretorio de configuracao do Jitsi
-mkdir -p /jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
+# Remove as configurações de Log do arquivo docker-compose.yml
+sed -i '/^[[:blank:]]*logging/d;s/#.*//' /home/azureuser/multicloud/jitsi-azure/compose-jvb/docker-compose.yml 
+sed -i '/^[[:blank:]]*driver/d;s/#.*//' /home/azureuser/multicloud/jitsi-azure/compose-jvb/docker-compose.yml
+sed -i '/^[[:blank:]]*options/d;s/#.*//' /home/azureuser/multicloud/jitsi-azure/compose-jvb/docker-compose.yml
+sed -i '/^[[:blank:]]*syslog/d;s/#.*//' /home/azureuser/multicloud/jitsi-azure/compose-jvb/docker-compose.yml
+sed -i '/^[[:blank:]]*tag/d;s/#.*//' /home/azureuser/multicloud/jitsi-azure/compose-jvb/docker-compose.yml
 
-# Instala o Docker
-apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt update
-apt install docker-ce docker-ce-cli containerd.io -y
-
-# Cria a rede jitsi-net no Docker
-docker network create jitsi-net
-
-# Instala o Docker Compose
-curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/sbin/docker-compose
-chmod +x /usr/local/sbin/docker-compose
-
-# Clona repositorio do github
-git clone -n https://github.com/roberto-farias/multicloud
-cd multicloud
-git checkout HEAD jitsi-azure
-
-# Inicia os containers do Jitsi
-cd jitsi-azure/compose-meet-jvb
+# Atualiza o container jitsi-jvb
+cd /home/azureuser/multicloud/jitsi-azure/compose-jvb/
 docker-compose up -d
