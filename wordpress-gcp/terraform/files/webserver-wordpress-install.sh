00f725fb2e9e6f89beac69c428cf258bb2234a2b
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2021-09-27 10:40:39 -0300

    Update webserver-wordpress-install.sh

--- wordpress-gcp/terraform/files/webserver-wordpress-install.sh
+++ wordpress-gcp/terraform/files/webserver-wordpress-install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Remove linhas comentadas do arquio docker-compose.yml
-sed '/^[[:blank:]]*#/d;s/#.*//' /home/suporte-gcp/multicloud/wordpress-gcp/compose-web/docker-compose.yml
+# Remove as configurações de Log do arquio docker-compose.yml
+sed -i '/^[[:blank:]]*logging/d;s/#.*//' /home/suporte-gcp/multicloud/wordpress-gcp/compose-web/docker-compose.yml
+sed -i '/^[[:blank:]]*driver/d;s/#.*//' /home/suporte-gcp/multicloud/wordpress-gcp/compose-web/docker-compose.yml
 
 # Atualiza o container webserver-wordpress
 cd /home/suporte-gcp/multicloud/wordpress-gcp/compose-web
