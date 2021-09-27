190ef5826cdc437cabb6cc6a82e4ec7adf751856
roberto-farias <67426067+roberto-farias@users.noreply.github.com>
2021-09-27 07:22:52 -0300

    Delete jitsi-azure/compose-torture directory

--- jitsi-azure/compose-torture/jitsi-torture.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./scripts/malleus.sh --conferences=1 --participants=2 --senders=2 --audio-senders=2 --duration=300 --room-name-prefix=test --hub-url=http://hub:4444/wd/hub --instance-url=https://jitsimulticloud.freedynamicdns.net/
